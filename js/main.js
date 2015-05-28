45fcdd3247b3d18711e1559389e5e1ef0f2e9894
villa7 <villa7.kluge5@gmail.com>
2015-05-28 14:33:15 -0400

    content

--- js/main.js
+++ js/main.js
@@ -1,42 +1,6 @@
 ﻿/*
 * Boop
 */
-var wordClock = {
-    init: function() {
-
-        switch (getQueryString('type')) {
-            case 'round':
-                $('.arc-seconds').lettering('seconds');
-                $('.arc-minutes').lettering('minutes');
-                $('.arc-hours').lettering('hours');
-                wordClockRound.init();
-                $('#pagestyle').attr('href', 'css/roundclock.css');
-                break;
-            case 'line':
-                $('.arc-seconds').lettering('secondsBR');
-                $('.arc-minutes').lettering('minutesBR');
-                $('.arc-hours').lettering('hoursBR');
-                wordClockLine.init();
-                $('#pagestyle').attr('href', 'css/lineclock.css');
-                break;
-            case 'square':
-                $('.arc-seconds').lettering('seconds');
-                $('.arc-minutes').lettering('minutes');
-                $('.arc-hours').lettering('hours');
-                wordClockBlock.init();
-                $('#pagestyle').attr('href', 'css/blockclock.css');
-                break;
-            default:
-                $('.arc-seconds').lettering('seconds');
-                $('.arc-minutes').lettering('minutes');
-                $('.arc-hours').lettering('hours');
-                wordClockRound.init();
-                $('#pagestyle').attr('href', 'css/roundclock.css');
-                break;
-        }
-    }
-} 
-
 getQueryString = function(name) {
     name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
     var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
