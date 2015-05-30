f81058a9f3c6deaedc245ededcb616952d6997f1
Theodore Kluge <villa7.kluge5@gmail.com>
2015-05-30 19:59:05 -0400

    layout

---
+++ arrays/js/cs.js
@@ -0,0 +1,20 @@
+﻿/*
+* Boop
+*/
+$('a[href*=#]:not([href=#])').click(function(e) {
+	//e.preventDefault();
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+	        $('html,body').animate({
+	            scrollTop: target.offset().top
+	        }, 1000, 'easeInOutCubic');
+	        //window.location.hash = target.selector;
+	        return false;
+        }
+    }
+});
+$('.nav div').click(function(e) {
+    $(e.target).children('a').click();
+});
