5ec0eda877654dc3f56d84feb2256bce65ce4963
Theodore Kluge <villa7.kluge5@gmail.com>
2015-06-05 23:59:54 -0400

    2d

--- arrays/js/cs.js
+++ arrays/js/cs.js
@@ -7,13 +7,21 @@ $('a[href*=#]:not([href=#])').click(function(e) {
         var target = $(this.hash);
         target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
         if (target.length) {
-	        $('html,body').animate({
-	            scrollTop: target.offset().top
-	        }, 1000, 'easeInOutCubic');
-	        //window.location.hash = target.selector;
+            $('html,body').animate({
+                scrollTop: target.offset().top
+            }, 1000, 'easeInOutCubic');
+            setTimeout(function() {
+	           window.location.hash = target.selector;
+               console.log(target.selector);
+            }, 1000);
 	        return false;
         }
     }
+});
+
+$('#col-nav').on('activate.bs.scrollspy', function () {
+  console.log($('#col-nav .active a').attr('href'));
+  //window.location.hash = $('#col-nav .active a').attr('href');
 });
 /*$('.nav div').click(function(e) {
     $(e.target).children('a').click();
